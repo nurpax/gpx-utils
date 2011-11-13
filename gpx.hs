62b39787097ca2297f4f159705ecef166f6e319e
Janne Hellsten <jjhellst@gmail.com>
2011-11-13 15:17:35 +0200

    compute track length

--- gpx.hs
+++ gpx.hs
@@ -2,6 +2,7 @@
 import Text.XML.HXT.Core
 import Data.Time (UTCTime, readTime)
 import Locale (defaultTimeLocale)
+import Text.Printf (printf)
 
 data Trkseg = Trkseg [Trkpt] deriving (Eq, Show)
 
@@ -17,10 +18,11 @@ readt :: String -> UTCTime
 readt = readTime defaultTimeLocale "%FT%T.000%Z"
 
 atTag tag = deep (isElem >>> hasName tag)
+text = getChildren >>> getText
 
 getTrkpt = atTag "trkpt" >>>
   proc x -> do
-    time_ <- getText <<< atTag "time" -< x
+    time_ <- text <<< atTag "time" -< x
     lon <- getAttrValue "lon" -< x
     lat <- getAttrValue "lat" -< x
     returnA -< Trkpt {
@@ -34,8 +36,25 @@ getTrkseg = atTag "trkseg" >>>
     segments <- listA getTrkpt -< x
     returnA -< Trkseg segments
 
+deg2rad x = 2 * pi * x / 360
+rad2deg x = x * 180 / pi
+
+segmentLength :: Trkpt -> Trkpt -> Double
+segmentLength a b =
+  let t = deg2rad $ longitude a - longitude b in
+  let d = (sin lat1) * (sin lat2) + (cos lat1) * (cos lat2) * (cos t) in
+  60.0 * 1.1515 * 1.609344 * (rad2deg $ acos d) where
+    lat1 = deg2rad $ latitude a
+    lat2 = deg2rad $ latitude b
+
+trackLength :: Trkseg -> Double
+trackLength (Trkseg segs) =
+  sum (map (uncurry $ segmentLength) segments) where
+    segments = zip segs (tail segs)
 
 main :: IO ()
 main =
---  runX (readDocument [withValidate no] "test/activity1_short.gpx" >>> getTrkseg)
-  putStrLn "foo"
+  do
+    trackSegs <- runX (readDocument [withValidate no] "test/activity1.gpx" >>> getTrkseg)
+    let len = trackLength $ head trackSegs
+    putStrLn (printf "Track length: %f" len)
